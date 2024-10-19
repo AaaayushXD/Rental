94441ff7a7dfa7a17b43e6aff1bf520816ab5183
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-10-19 11:18:23 +0545

    update: middleware to access token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -99,5 +99,5 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,21 @@ export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  console.log(token);
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
   const url = request.nextUrl;
 
   if (
     token &&
-    (url.pathname.startsWith("sign-in") ||
-      url.pathname.startsWith("sign-up") ||
-      url.pathname.startsWith("verify"))
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-  if (!token && url.pathname.startsWith("dashboard")) {
+  if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
   return NextResponse.next();
