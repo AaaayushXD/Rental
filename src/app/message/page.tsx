5a8368c94413e321342428799c929433c944d23f
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-10-22 14:13:38 +0545

    ui:complete  message ui

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-// import MessagePreviewCard from "@/components/cards/MessagePreviewCard";
+import MessagePreviewCard from "@/components/cards/MessagePreviewCard";
 import {
   ChevronLeft,
   Images,
@@ -14,51 +14,57 @@ import {
 import { Avatar } from "@radix-ui/react-avatar";
 import { AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
+import MessageCard from "@/components/cards/MessageCard";
 
 const Page = () => {
   return (
-    <div className="min-w-[100vw] w-full h-full min-h-screen text-brandCopy">
+    <div className="min-w-[90vw] w-full h-full min-h-screen text-brandCopy">
       <Navbar />
-      <div className="flex w-full h-full justify-center items-center p-5 bg-brandForeground ">
-        {/* <div className="max-w-[1800px] w-full  flex flex-col font-light gap-10 pt-5">
-          <h3 className="text-xl tracking-wider font-extrabold pl-3">
-            Messages
-          </h3>
-          <div className="px-3 flex flex-col gap-2 justify-start w-full">
-            <MessagePreviewCard />
-            <MessagePreviewCard />
-            <MessagePreviewCard />
-          </div>
-        </div> */}
-        <div className="max-w-[1800px] min-h-screen flex flex-col w-full font-light gap-5 pt-5">
-          <div className="flex w-full justify-between items-center">
-            <div className="flex justify-start items-center gap-2">
-              <ChevronLeft />
-              <Avatar className="size-[40px] flex justify-center items-center">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  className="rounded-full"
-                />
-              </Avatar>
-              <p className="text-sm font-medium">Aayush Lamichhane</p>
-            </div>
-            <div className="flex justify-start items-center gap-3">
-              <Phone />
-              <Video />
-              <MoreVertical />
+      <div className="flex w-full h-full justify-center items-start p-5 bg-brandForeground overflow-x-hidden">
+        <div className="max-w-[1800px] flex justify-start items-start gap-8 w-full overflow-x-hidden">
+          <div className="w-full lg:max-w-[600px] flex flex-col font-light gap-10 pt-5 h-full ">
+            <h3 className="text-xl tracking-wider font-extrabold pl-3">
+              Messages
+            </h3>
+            <div className="px-3 flex flex-col gap-2 justify-start w-full max-h-screen overflow-y-auto">
+              <MessagePreviewCard />
+              <MessagePreviewCard />
+              <MessagePreviewCard />
             </div>
           </div>
-          <Separator />
-          <div className="w-full min-h-[80vh] h-full overflow-y-auto"></div>
-          <Separator />
-          <div className="flex w-full gap-3 justify-center items-center">
-            <Images />
-            <input
-              placeholder="Send message"
-              type="text"
-              className="bg-transparent border py-2 px-4 rounded-xl flex-grow text-sm"
-            />
-            <Send />
+          <div className="max-w-[1800px] max-h-screen lg:flex flex-col w-full font-light gap-5 pt-5 hidden overflow-x-hidden">
+            <div className="flex w-full justify-between items-center">
+              <div className="flex justify-start items-center gap-2">
+                <ChevronLeft />
+                <Avatar className="size-[40px] flex justify-center items-center">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    className="rounded-full"
+                  />
+                </Avatar>
+                <p className="text-sm font-medium">Aayush Lamichhane</p>
+              </div>
+              <div className="flex justify-start items-center gap-3">
+                <Phone />
+                <Video />
+                <MoreVertical />
+              </div>
+            </div>
+            <Separator />
+            <div className="w-full min-h-[60vh] h-full overflow-y-auto flex flex-col gap-1">
+              <MessageCard />
+              <MessageCard />
+            </div>
+            <Separator />
+            <div className="flex w-full gap-3 justify-center items-center">
+              <Images />
+              <input
+                placeholder="Send message"
+                type="text"
+                className="bg-transparent border py-2 px-4 rounded-xl flex-grow text-sm"
+              />
+              <Send />
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/cards/MessageCard.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const MessageCard = () => {
+  return (
+    <div className="max-w-[350px] w-fit p-3 rounded-r-lg rounded-tl-xl text-sm border">
+      Hello, How are you?
+    </div>
+  );
+};
+
+export default MessageCard;
